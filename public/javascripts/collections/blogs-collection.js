d15b0e2b99e4c2e8475b897f33820bbe518c6319
Andy Babbitt <ababbitt@peapod.com>
2013-05-31 11:31:42 -0500

    added post

--- public/javascripts/collections/blogs-collection.js
+++ public/javascripts/collections/blogs-collection.js
@@ -2,8 +2,8 @@ var BlogsCollection = Backbone.Collection.extend({
 
 	model: BlogModel,
 
-	url: 'data/data0513.json'
-	//url:'127.0.0.1:3000/blogging'
+	url: 'data/data0528.json'
+	//url:'/blogging'
 
 });
 
