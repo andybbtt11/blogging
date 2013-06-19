6117c352e488898a4dcde05249ab86d12f80099b
Matt Lo <mattlo.developer@gmail.com>
2013-06-19 14:31:18 -0500

    added index.html support using EJS tpl engine

--- app.js
+++ app.js
@@ -7,9 +7,18 @@ var express = require('express'),
 	routes = require('./routes'),
 	http = require('http'),
 	path = require('path'),
+	engine = require('ejs-locals'),
 	app = express();
 
 app.configure(function(){
+	// map .renderFile to ".html" files
+	app.engine('ejs', engine);
+	app.engine('html', require('ejs').renderFile);
+
+	// make ".html" the default
+	app.set('views', __dirname);
+	app.set('view engine', 'html');
+	
 	app.set('port', process.env.PORT || 3000);
 	app.use(express.favicon());
 	app.use(express.logger('dev'));
@@ -23,13 +32,9 @@ app.configure('development', function(){
 	app.use(express.errorHandler());
 });
 
-app.get('/', function(){
- 
+app.get('/', function(request, response) {
+	response.render('index.html');
 });
-
-app.get('/blogging', routes.posts.all);
-app.get('/blogging/:id', routes.posts.one);
-app.post('/blogging', routes.posts.create);
 
 http.createServer(app).listen(app.get('port'), function(){
 	console.log("Express server listening on port " + app.get('port'));
