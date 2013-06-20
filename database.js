c2eeb23f7aadc58c2190dfcffbd369e82c89ac56
Andy Babbitt <ababbitt@peapod.com>
2013-06-20 15:15:23 -0500

    removed mongoose

--- database.js
+++ database.js
@@ -1,9 +1,6 @@
-//var dbUrl = "blog";
-var dbUrl = "mongodb://blog:lKHLuxs5m8zr0nzbKrd4VlFY6jx8ZFRTd3hLC864zy0-@ds027748.mongolab.com:27748/blog";
+var dbUrl = "blog";
 var collections = ["posts"];
 
-var connectionString = process.env.CUSTOMCONNSTR_MONGOLAB_URI;
-mongoose.connect(connectionString);
 
 var db = require("mongojs").connect(dbUrl, collections);
 
