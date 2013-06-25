32430666db2e13fd9f89e8949511a82f5d066295
Andy Babbitt <ababbitt@peapod.com>
2013-06-25 17:04:43 -0500

    db

--- database.js
+++ database.js
@@ -1,4 +1,4 @@
-var dbUrl = "mongodb://andybbtt11:lllllll8@ds027748.mongolab.com:27748/blog";
+var dbUrl = "mongodb://genericuser:genericpw@ds027748.mongolab.com:27748/blog";
 var collections = ["posts"];
 
 var db = require("mongojs").connect(dbUrl, collections);
