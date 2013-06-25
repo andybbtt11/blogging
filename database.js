687ea78918df9ff8c9789590dc624cf0e5acbe89
Andy Babbitt <ababbitt@peapod.com>
2013-06-25 12:09:00 -0500

    db change

--- database.js
+++ database.js
@@ -1,7 +1,6 @@
 var dbUrl = "mongodb://genericuser:genericpw@ds027748.mongolab.com:27748/blog";
 var collections = ["posts"];
 
-console.log(dbUrl, collections);
 var db = require("mongojs").connect(dbUrl, collections);
 
 
